436bcc2c49b330436312a4ecb2d4b32e497f8287
Maximilian Seebacher <max@seebacher.online>
2024-01-24 21:52:47 +0100

    Updated Code, fixed some bugs

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const https = require("https"); // Import HTTPS module
 const fs = require("fs");
 const path = require("path");
+const axios = require("axios"); // Axios for HTTP requests
 const loki = require("lokijs"); // LokiJS for database
 const multer = require("multer");
 const WebSocket = require("ws");
@@ -12,36 +13,43 @@ console.log("-----------------------------");
 console.log("Server starting...");
 
 // SSL Certificate and Private Key paths
-const privateKey = fs.readFileSync("/etc/letsencrypt/live/vps-81d09b41.vps.ovh.net/privkey.pem", "utf8");
-const certificate = fs.readFileSync("/etc/letsencrypt/live/vps-81d09b41.vps.ovh.net/fullchain.pem", "utf8");
+const privateKey = fs.readFileSync(
+  "/etc/letsencrypt/live/vps-81d09b41.vps.ovh.net/privkey.pem",
+  "utf8"
+);
+const certificate = fs.readFileSync(
+  "/etc/letsencrypt/live/vps-81d09b41.vps.ovh.net/fullchain.pem",
+  "utf8"
+);
 const credentials = { key: privateKey, cert: certificate };
 
 // Code to handle file uploads (PNG)
 const upload = multer({
-  dest: "tmp/", // Temp folder for uploads
+  dest: "tmp/", // Temp folder for checking uploads
   fileFilter: (req, file, cb) => {
-    if (file.mimetype === "image/png") {
-      cb(null, true); // accept png files
-    } else {
-      cb(null, false); // reject other file types
-    }
+      if (file.mimetype === "image/png") {
+          cb(null, true); // accept png files
+      } else {
+          cb(null, false); // reject other file types
+      }
   },
   limits: {
-    fileSize: 10 * 1024 * 1024, // max filesize is 10MB
+      fileSize: 10 * 1024 * 1024, // max filesize is 10MB
   },
 });
 
-const apiKey = 'dcfc58543cd6c3cc1f73d8eef31a1cb077393bbebbf28c8c8fb1b69e11ca2167'; // correct API-KEY
+const apiKey =
+  "dcfc58543cd6c3cc1f73d8eef31a1cb077393bbebbf28c8c8fb1b69e11ca2167"; // correct API-KEY
 
 const checkApiKey = (req, res, next) => {
-  const receivedApiKey = req.headers['x-api-key'];
+  const receivedApiKey = req.headers["x-api-key"];
 
   if (receivedApiKey && receivedApiKey === apiKey) {
     next(); // API-Key is correct, continue
-    console.log("API-Key correct");
+    //console.log("API-Key correct");
   } else {
-    console.log("API-Key incorrect");
-    res.status(401).json({ error: 'Not authorized' }); // API-Key incorrect or not sent, not authorized
+    //console.log("API-Key incorrect");
+    res.status(401).json({ error: "Not authorized" }); // API-Key incorrect or not sent, not authorized
   }
 };
 
@@ -72,6 +80,7 @@ function broadcastMessage(message) {
       client.send(message);
     }
   });
+  //console.log("Broadcasted message:", message);
 }
 
 // Connect to LokiJS DB
@@ -92,13 +101,93 @@ app.get("/get-data", (req, res) => {
   res.json(result);
 });
 
+// Route to get image from tadeot server
+app.get("/get-user-image", async (req, res) => {
+  try {
+    const { filename } = req.query;
+
+    // Check if the "filename" parameter is present
+    if (!filename) {
+      return res.status(400).json({ error: 'Parameter "filename" is missing' });
+    }
+
+    // Check if the filename has a ".png" extension
+    if (!filename.endsWith(".png")) {
+      return res.status(400).json({
+        error: 'Invalid file extension. Only ".png" files are supported.',
+      });
+    }
+
+    // URL of the tadeot image (hidden from public)
+    const imageUrl = `https://tadeot.htl-leonding.ac.at/tadeot-backend-v23/images/${filename}`;
+
+    // Check if the file exists
+    const imageExists = await checkFileExists(imageUrl);
+
+    // If the file exists, send the image back
+    if (imageExists) {
+      const imageStream = await axios.get(imageUrl, { responseType: "stream" });
+      imageStream.data.pipe(res);
+    } else {
+      // If the file doesn't exist, send the default file from the server
+      const defaultImageStream = fs.createReadStream(
+        "./public/media/userpic-Placeholder.jpg"
+      );
+      defaultImageStream.pipe(res);
+    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+// Function to check if the file exists
+async function checkFileExists(url) {
+  try {
+    const response = await axios.head(url);
+    return response.status === 200;
+  } catch (error) {
+    return false;
+  }
+}
+
+// generate Unique Playername if none was sent
+function generateUniquePlayerCode() {
+  const uniqueDigits = new Set();
+
+  while (uniqueDigits.size < 4) {
+    const randomDigit = Math.floor(Math.random() * 10);
+    uniqueDigits.add(randomDigit);
+  }
+
+  const uniqueCode = Array.from(uniqueDigits).join("");
+  return `Player ${uniqueCode}`;
+}
+
 // Route to receive data for the highscore table
 app.post("/add-data", checkApiKey, (req, res) => {
   const newData = req.body;
 
   if (newData) {
+    console.log("Received data:", newData);
+    newData.tadeotId = newData.tadeotId.toString().padStart(4, "0"); // Pad tadeotId with leading zeros to 4 digits
+    const imageURL =
+      "https://vps-81d09b41.vps.ovh.net/get-user-image?filename=Visitor_" +
+      newData.tadeotId +
+      ".png"; //Build url to image, by tadeotId
+    newData.image = imageURL;
+
+    // Check if the name is empty, if so, set it to the name generated in generateUniquePlayerCode()
+    if (
+      newData.name == "" ||
+      newData.name == null ||
+      newData.name == undefined
+    ) {
+      newData.name = generateUniquePlayerCode();
+    }
     collection.insert(newData); // Save new data to the database
     db.saveDatabase(); // Save changes to the database
+    broadcastMessage("updateHighscores"); // Send message to all clients to update the highscore table
     res.status(201).json({ message: "Data saved successfully!" });
   } else {
     res.status(400).json({ error: "Invalid request!" });
@@ -121,7 +210,7 @@ app.post("/saveImage", checkApiKey, upload.single("image"), (req, res) => {
       console.log("Error in saving the file:", err);
       return res.status(500).json({ error: "Error saving image!" });
     }
-    res.status(200).json({ message: "Image saved successfully!" });
+    res.status(201).json({ message: "Image saved successfully!" });
 
     //Send file to all clients
     fs.readFile(targetPath, (err, data) => {
@@ -129,7 +218,7 @@ app.post("/saveImage", checkApiKey, upload.single("image"), (req, res) => {
         console.log("Error in reading the file:", err);
       }
       broadcastMessage(data);
-      console.log("Broadcasted file...");
+      fs.unlinkSync(targetPath); // Delete the file after sending it
     });
   });
 });
--- backend/public/index.js
+++ backend/public/index.js
@@ -1,3 +1,29 @@
+function fetchHighscores() {
+  fetch("/get-data")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data) {
+        document.querySelector("tbody").innerHTML = ""; // Clear Table before adding data
+        data.sort((a, b) => b.score - a.score);
+        data.forEach(function (item, index) {
+          document.querySelector("tbody").innerHTML += `
+                      <tr>
+                          <td>${index + 1}</td>
+                          <td><img src="${
+                            item.image
+                          }" alt="UserImage" class="img-thumbnail"></td>
+                          <td>${item.name}</td>
+                          <td>${item.score}</td>
+                      </tr>`;
+        });
+        console.log("Highscores fetched successfully");
+      } else {
+        console.error("Error fetching data");
+      }
+    })
+    .catch((error) => console.error("Error:", error));
+}
+
 //Load the Data for the Highscore Table
 document.addEventListener("DOMContentLoaded", function () {
   const socket = new WebSocket("wss://vps-81d09b41.vps.ovh.net");
@@ -6,42 +32,27 @@ document.addEventListener("DOMContentLoaded", function () {
     socket.send("Hello Server!");
   });
 
-  socket.addEventListener('message', function(event) {
+  socket.addEventListener("message", function (event) {
     const data = event.data;
 
-    if (data instanceof Blob) {
-        const imageUrl = URL.createObjectURL(data);
+    if (data instanceof Blob) { // Change Image when received new one
+      const reader = new FileReader();
+
+      reader.onload = function () {
+        const imageUrl = reader.result;
         const img = document.getElementById("vrImage");
         img.src = imageUrl;
-        img.onload = () => {
-            URL.revokeObjectURL(imageUrl);
-        };
+
         console.log("Image received and updated");
-    }else{
-      console.log("Message received from Server: "+data);
+      };
+      reader.readAsDataURL(data);
+    } else if(data == "updateHighscores"){
+      fetchHighscores(); // Update Highscores on server request
+    } else {
+      console.log("Message received from Server: " + data);
     }
-});
+  });
 
   //Get Data for Highscore Table
-  fetch("/get-data")
-    .then((response) => response.json())
-    .then((data) => {
-      if (data) {
-        data.sort((a, b) => b.score - a.score);
-        data.forEach(function (item, index) {
-          document.querySelector("tbody").innerHTML += `
-                      <tr>
-                          <td>${index + 1}</td>
-                          <td><img src="./media/${
-                            item.image
-                          }" alt="UserImage" class="img-thumbnail"></td>
-                          <td>${item.name}</td>
-                          <td>${item.score}</td>
-                      </tr>`;
-        });
-      } else {
-        console.error("Error fetching data");
-      }
-    })
-    .catch((error) => console.error("Error:", error));
+  fetchHighscores();
 });
