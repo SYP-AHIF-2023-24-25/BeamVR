7f1234b05f9ab652fcfba3e21ce34562ea1ccf37
elplak <elplak19@gmail.com>
2024-04-22 01:24:32 +0200

    new version / update all

--- beamVR-frontend/src/app/app.module.ts
+++ beamVR-frontend/src/app/app.module.ts
@@ -1,26 +1,33 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from "@angular/forms";
-import { RouterModule } from "@angular/router";
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import {MainComponent} from "./main/main.component";
+import {HttpClientModule} from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {LoginComponent} from './login/login.component';
+import {ScoresComponent} from './scores/scores.component';
+import {FormsModule} from "@angular/forms";
+import {NgOptimizedImage} from "@angular/common";
+import {HighestLatestScores} from "./highest-latest-scores/highest-latest-scores.component";
+import {AdminComponent} from "./admin/admin.component";
 
 @NgModule({
-  declarations: [
-    MainComponent,
-    AppComponent,
-    LoginComponent,
-  ],
-  imports: [
-    BrowserModule,
-    RouterModule,
-    HttpClientModule,
-    FormsModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        LoginComponent,
+        ScoresComponent,
+        HighestLatestScores,
+        AdminComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        HttpClientModule,
+        FormsModule,
+        NgOptimizedImage,
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule {}
+export class AppModule {
+}
--- beamVR-frontend/src/app/login/login.component.ts
+++ beamVR-frontend/src/app/login/login.component.ts
@@ -1,80 +1,100 @@
-import { Component, OnInit, AfterViewInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Router } from '@angular/router';
+import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit, AfterViewInit {
-  username: string = '';
-  password: string = '';
-  errorMessage: string = '';
-  isLoggedIn: boolean = false;
-  showPassword: boolean = false;
+    username: string = '';
+    password: string = '';
+    errorMessage: string = '';
+    isLoggedIn: boolean = false;
+    showPassword: boolean = false;
 
-  constructor(private http: HttpClient, private router: Router) {}
-
-  ngOnInit(): void {
-    this.checkSession();
-  }
-
-  ngAfterViewInit(): void {
-    this.setInitialTheme();
-  }
-
-  togglePassword(): void {
-    this.showPassword = !this.showPassword;
-  }
-
-  async onSubmit(): Promise<void> {
-    const hashedPassword = await this.hashPassword(this.password);
-    const data = { username: this.username, passwordHash: hashedPassword };
-
-    this.http.post("https://vps-81d09b41.vps.ovh.net/loginAuth", data, { withCredentials: true })
-      .subscribe({
-        next: (response: any) => this.handleLoginSuccess(response),
-        error: (error) => this.handleLoginError(error)
-      });
-  }
-
-  async hashPassword(password: string): Promise<string> {
-    const encoder = new TextEncoder();
-    const buffer = await crypto.subtle.digest("SHA-256", encoder.encode(password));
-    return Array.from(new Uint8Array(buffer), byte => byte.toString(16).padStart(2, '0')).join('');
-  }
-
-  checkSession(): void {
-    this.http.get("https://vps-81d09b41.vps.ovh.net/checkSession", { withCredentials: true })
-      .subscribe({
-        next: (response) => this.isLoggedIn = true,
-        error: (error) => this.isLoggedIn = false
-      });
-  }
-
-  changeThemeColor(): void {
-    const body = document.body;
-    body.classList.toggle("dark-theme");
-    document.cookie = `darkmode=${body.classList.contains('dark-theme')}; path=/; max-age=31536000`;
-  }
-
-  private handleLoginSuccess(response: any): void {
-    document.cookie = `authToken=${response.sessionID}; path=/; samesite=strict`;
-    this.router.navigate(["/admin"]);
-  }
-
-  private handleLoginError(error: any): void {
-    if (error.status === 401) {
-      this.errorMessage = "Unauthorized: Invalid username or password.";
-    } else {
-      this.errorMessage = "An unexpected error occurred. Please try again later.";
+    constructor(private http: HttpClient, private router: Router) {
     }
-  }
 
-  private setInitialTheme(): void {
-    if (document.cookie.includes("darkmode=true")) {
-      document.body.classList.add("dark-theme");
+    ngOnInit(): void {
+        this.checkSession();
     }
-  }
+
+    ngAfterViewInit(): void {
+        this.checkDarkMode();
+    }
+
+    togglePassword(): void {
+        this.showPassword = !this.showPassword;
+    }
+
+    async onSubmit(): Promise<void> {
+        try {
+            const hashedPassword = await this.hashPassword(this.password);
+            const data = {username: this.username, passwordHash: hashedPassword};
+
+            this.http.post("https://vps-81d09b41.vps.ovh.net/loginAuth", data).subscribe(
+                (response: any) => {
+                    document.cookie = `authToken=${response.sessionID}; path=/; samesite=strict`;
+                    this.router.navigate(['/admin']);
+                },
+                (error) => {
+                    console.error(error);
+                    if (error.status === 401) {
+                        this.errorMessage = "Unauthorized: Invalid username or password.";
+                    } else {
+                        this.errorMessage = "An unexpected error occurred. Please try again later.";
+                    }
+                }
+            );
+        } catch (error) {
+            console.error("Error hashing password:", error);
+        }
+    }
+
+    async hashPassword(password: string): Promise<string> {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(password);
+        const buffer = await crypto.subtle.digest("SHA-256", data);
+        const hashedPassword = Array.from(new Uint8Array(buffer), (byte) => byte.toString(16).padStart(2, '0')).join('');
+        return hashedPassword;
+    }
+
+    checkSession(): void {
+        fetch("https://vps-81d09b41.vps.ovh.net/checkSession", {
+            method: "GET",
+            credentials: "include"
+        }).then((res) => {
+            if (res.status === 200) {
+                this.isLoggedIn = true;
+                this.router.navigate(['/admin']);
+            } else {
+                this.isLoggedIn = false;
+            }
+        }).catch(() => {
+            this.isLoggedIn = false;
+        });
+    }
+
+    changeThemeColor(): void {
+        const body = document.getElementsByTagName("body")[0];
+        if (body.classList.contains("dark-theme")) {
+            body.classList.remove("dark-theme");
+            document.cookie = `darkmode=false; path=/; max-age=31536000`;
+        } else {
+            body.classList.add("dark-theme");
+            document.cookie = `darkmode=true; path=/; max-age=31536000`;
+        }
+    }
+
+    checkDarkMode(): void {
+        if (document.cookie.includes("darkmode=true")) {
+            document.getElementsByTagName("body")[0].classList.add("dark-theme");
+            let formLabels = document.getElementsByClassName("form-label");
+            for (let i = 0; i < formLabels.length; i++) {
+                (formLabels[i] as HTMLElement).style.color = "white";
+            }
+        }
+    }
 }
