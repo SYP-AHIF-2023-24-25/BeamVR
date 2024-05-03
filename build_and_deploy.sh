b838cb25bf999fd36c5f82fea169846a6a2fe83f
Steverixxi <stefanwerner05@gmail.com>
2024-05-03 08:37:10 +0200

    build and deploy script logs

--- build_and_deploy.sh
+++ build_and_deploy.sh
@@ -1,3 +1,5 @@
+echo "Building and deploying BeamVR app"
 docker rm -f beamvr-app
 docker build -t beamvr-app .
 docker run -d -p 80:80 --name=beamvr-app --restart=always beamvr-app
+echo "BeamVR app deployed successfully, localhost:80 is now serving the app."
