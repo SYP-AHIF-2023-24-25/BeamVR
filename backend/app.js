36eb9ed3822b4f14a7ca9ea208cc540ff840a3bf
elplak <elplak19@gmail.com>
2024-05-28 08:40:52 +0200

    admin authorization

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,37 @@ const express = require("express");
 const { createTable, db, closeDb } = require('./database');
 const cors = require('cors');
 const socketIo = require('socket.io');
+const session = require('express-session');
+const Keycloak = require('keycloak-connect');
+const fs = require("fs");
 
 const app = express();
 const port = 3000;
+const memoryStore = new session.MemoryStore();
+const keycloak = new Keycloak({ store: memoryStore });
 
 app.use(cors());
 app.use(express.json());
+app.use(session({
+    secret: 'some secret',
+    resave: false,
+    saveUninitialized: true,
+    store: memoryStore
+}));
+app.use(keycloak.middleware());
+
+app.get('/protected', [keycloak.protect()], (req, res) => {
+    const token = req.kauth.grant.access_token.content;
+    let lines = fs.readFileSync('trustedUsers.txt').toString().split('\n');
+
+    for (let i = 0; i < lines.length; i++) {
+        if (lines[i] === token.name) {
+            res.sendStatus(200);
+            return;
+        }
+    }
+    res.sendStatus(403);
+});
 
 async function manageDatabase() {
     try {
--- beamVR-frontend/src/app/login/login.component.ts
+++ beamVR-frontend/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {KeycloakService} from "keycloak-angular";
-import {Router} from '@angular/router';
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "../../environment/environment";
 
 @Component({
     selector: 'app-login',
@@ -9,21 +10,24 @@ import {Router} from '@angular/router';
 })
 export class LoginComponent {
     isLoggedIn = false;
+    isAuthorized = false;
     errorMessage: string = '';
 
-    constructor(private keycloakService: KeycloakService) {
+    constructor(private keycloakService: KeycloakService, private http: HttpClient) {
         this.isLoggedIn = this.keycloakService.isLoggedIn();
-        this.keycloakService.getToken().then(token => {
-            console.log(token);
+        this.keycloakService.getToken().then(async token => {
+            if (token) {
+                this.getProtectedResource();
+            }
         });
     }
 
     async login(): Promise<void> {
         if (this.isLoggedIn) {
             return
+
         }
         await this.keycloakService.login();
-
     }
 
     async logout(): Promise<void> {
@@ -31,5 +35,23 @@ export class LoginComponent {
             return;
         }
         await this.keycloakService.logout();
+    }
+
+   getProtectedResource() {
+        this.keycloakService.getToken().then(token => {
+            const headers = new HttpHeaders().set('Authorization', 'Bearer ' + token);
+            this.http.get(environment.apiBaseUrl + 'protected', { headers, responseType: 'text' })
+                .subscribe({
+                    next: result => {
+                        this.isAuthorized = true;
+                        console.log(result);
+                    },
+                    error: error => {
+                        this.errorMessage = 'Not authorized to access this resource';
+                        this.isAuthorized = false;
+                        console.error(error);
+                    }
+                });
+        });
     }
 }
