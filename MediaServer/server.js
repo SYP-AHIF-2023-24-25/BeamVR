5365de9e7c1d1a072b98314138cc3de7067fd94f
Steverixxi <61599057+Steverixxi@users.noreply.github.com>
2024-06-10 14:59:29 +0200

    Update server.js

--- MediaServer/server.js
+++ MediaServer/server.js
@@ -51,5 +51,5 @@ const app = express();
 const port = 3000;
 app.use(express.static(__dirname + '/public'));
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`MediaServer listening at http://localhost:${port}`);
 });
